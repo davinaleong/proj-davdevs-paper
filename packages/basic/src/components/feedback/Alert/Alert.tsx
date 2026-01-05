1bec461c4745360416e4359e51723b17c5be89a3
Davina Leong <leong.shi.yun@gmail.com>
2026-01-05 15:47:33 +0800

    Fixed the Alert dark mode bugs

--- packages/basic/src/components/feedback/Alert/Alert.tsx
+++ packages/basic/src/components/feedback/Alert/Alert.tsx
@@ -48,10 +48,10 @@ export const Alert = React.forwardRef<AlertRef, AlertProps>(
 
     // Variant classes
     const variantClasses = {
-      info: 'bg-blue-50 border-blue-200 text-blue-800',
-      success: 'bg-green-50 border-green-200 text-green-800',
-      warning: 'bg-yellow-50 border-yellow-200 text-yellow-800',
-      error: 'bg-red-50 border-red-200 text-red-800'
+      info: 'bg-blue-50 border-blue-200 text-blue-800 dark:bg-blue-900 dark:border-blue-700 dark:text-blue-300',
+      success: 'bg-green-50 border-green-200 text-green-800 dark:bg-green-900 dark:border-green-700 dark:text-green-300',
+      warning: 'bg-yellow-50 border-yellow-200 text-yellow-800 dark:bg-yellow-900 dark:border-yellow-700 dark:text-yellow-300',
+      error: 'bg-red-50 border-red-200 text-red-800 dark:bg-red-900 dark:border-red-700 dark:text-red-300'
     };
 
     // Elevation classes (shadow)
